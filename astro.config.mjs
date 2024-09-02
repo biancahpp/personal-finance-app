e2be5d09bfccac861abcb9fd72a7e5e901496b69
Bianca Procopio <biancahprocopio@gmail.com>
2024-09-02 13:06:19 -0600

    undo config and verify manually

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://master--gleeful-torrone-9760d6.netlify.app',
-  integrations: [tailwind(), icon()],
-  trailingSlash: 'always'
+  integrations: [tailwind(), icon()]
 })
